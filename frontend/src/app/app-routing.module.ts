2e2dbb96b6c14a260e80d89eb1d6763b6736105f
lpeppe <lcpp1994@gmail.com>
2019-12-22 15:35:27 +0100

    user and add-user components created

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
+import { UsersComponent } from "./users/users.component";
+import { AddUserComponent } from "./add-user/add-user.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "users", component: UsersComponent },
+  { path: "addUser", component: AddUserComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -11,9 +11,11 @@ import { MatButtonModule } from "@angular/material/button";
 import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatIconModule } from "@angular/material/icon";
 import { MatListModule } from "@angular/material/list";
+import { UsersComponent } from "./users/users.component";
+import { AddUserComponent } from './add-user/add-user.component';
 
 @NgModule({
-  declarations: [AppComponent, MainNavComponent],
+  declarations: [AppComponent, MainNavComponent, UsersComponent, AddUserComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,

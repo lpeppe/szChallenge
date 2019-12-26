e58bcb82c4d527b18b32e0e6dfc72d4e3706ba0d
lpeppe <lcpp1994@gmail.com>
2019-12-26 11:30:18 +0100

    backend endpoint updated

--- backend/app/Controllers/Http/UserController.js
+++ backend/app/Controllers/Http/UserController.js
@@ -3,7 +3,14 @@ const User = use("App/Models/User");
 
 class UserController {
   getUsers() {
-    return User.all();
+    return User.all().then(users =>
+      users.toJSON().map(({ first_name, surname, date_of_birth, email }) => ({
+        firstName: first_name,
+        surname,
+        dateOfBirth: date_of_birth,
+        email
+      }))
+    );
   }
 }
 

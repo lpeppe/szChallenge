4bfb1bd9a7c35dabb7887563393a5b292604b332
lpeppe <lcpp1994@gmail.com>
2019-12-26 12:08:17 +0100

    pagination implemented

--- backend/app/Controllers/Http/UserController.js
+++ backend/app/Controllers/Http/UserController.js
@@ -1,16 +1,19 @@
 "use strict";
 const User = use("App/Models/User");
+const Database = use("Database");
 
 class UserController {
-  getUsers() {
-    return User.all().then(users =>
-      users.toJSON().map(({ first_name, surname, date_of_birth, email }) => ({
-        firstName: first_name,
-        surname,
-        dateOfBirth: date_of_birth,
-        email
-      }))
-    );
+  getUsers({ request }) {
+    const { pageIndex, pageSize } = request.all();
+    return Database.from("users")
+      .select(
+        "id",
+        "first_name as firstName",
+        "surname",
+        "date_of_birth as dateOfBirth",
+        "email"
+      )
+      .paginate(pageIndex, pageSize);
   }
 }
 

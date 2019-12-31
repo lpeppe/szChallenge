21d4af2f5c79c78f064168d21242bb7d11e3cd23
lpeppe <lcpp1994@gmail.com>
2019-12-31 12:41:48 +0100

    added future dates detection in backend date validation

--- backend/providers/DateValidationProvider.js
+++ backend/providers/DateValidationProvider.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const { ServiceProvider } = require("@adonisjs/fold");
+const moment = require("moment");
 
 class DateValidationProvider extends ServiceProvider {
   /**
@@ -38,6 +39,8 @@ class DateValidationProvider extends ServiceProvider {
     }
     const dateRegex = /(^(((0[1-9]|1[0-9]|2[0-8])[/](0[1-9]|1[012]))|((29|30|31)[/](0[13578]|1[02]))|((29|30)[/](0[4,6,9]|11)))[/](19|[2-9][0-9])\d\d$)|(^29[/]02[/](19|[2-9][0-9])(00|04|08|12|16|20|24|28|32|36|40|44|48|52|56|60|64|68|72|76|80|84|88|92|96)$)/;
     if (!dateRegex.test(value)) throw message;
+    // throw error if date passed is in the future
+    if (moment(value, "DD/MM/YYYY").isAfter(moment())) throw message;
   }
 }
 

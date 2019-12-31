5eafbb995929c0b0d498f6ccb0bdae8609fe111c
lpeppe <lcpp1994@gmail.com>
2019-12-31 12:42:00 +0100

    updated seeding factory

--- backend/database/factory.js
+++ backend/database/factory.js
@@ -14,12 +14,19 @@
 /** @type {import('@adonisjs/lucid/src/Factory')} */
 const Factory = use("Factory");
 const moment = require("moment");
+// create an array of years to use later on to generate random
+// years
+const years = Array(30)
+  .fill(0)
+  .map((_, i) => i + 1980);
 
 Factory.blueprint("App/Models/User", faker => {
   return {
     first_name: faker.first(),
     surname: faker.last(),
-    date_of_birth: moment(faker.date()).format("DD/MM/YYYY"),
+    date_of_birth: moment(
+      faker.date({ year: years[Math.floor(Math.random() * 30)] })
+    ).format("DD/MM/YYYY"),
     email: faker.email()
   };
 });
